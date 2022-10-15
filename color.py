564edf3f393a552cec0b0e1c8c5bb408d951e52f
Kiwi <88976806+TH1RDKEYBLADE@users.noreply.github.com>
2022-10-15 16:51:34 -0400

    Added "Font / Style" Category

--- color.py
+++ color.py
@@ -3,3 +3,11 @@
 REM_STYLE = "\33[0m"
 REM_CURSOR = "\033[?25l"
 ADD_CURSOR = "\033[?25h"
+
+# Font / Style
+BOLD = "\33[1m"
+ITALIC = "\33[3"
+UNDERLINE = "\33[4m"
+BLINK = "\33[5m"
+BLINK2 = "\33[6m"
+HIGHLIGHT = "\33[7m"
