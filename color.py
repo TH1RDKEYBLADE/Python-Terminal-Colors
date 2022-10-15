28009d8d92f494372b42790b25f990033a8d2142
Kiwi <88976806+TH1RDKEYBLADE@users.noreply.github.com>
2022-10-15 16:52:33 -0400

    Added Colors to the "Foreground" Category

--- color.py
+++ color.py
@@ -11,3 +11,31 @@ UNDERLINE = "\33[4m"
 BLINK = "\33[5m"
 BLINK2 = "\33[6m"
 HIGHLIGHT = "\33[7m"
+
+# Red (ForeGround)
+PINK = "\33[91m"
+RED = "\33[31m"
+
+# Green (ForeGround)
+LIME = "\33[92m"
+GREEN = "\33[32m"
+
+# Blue (ForeGround)
+AQUA = "\33[96m"
+BLUE = "\33[94m"
+CYAN = "\33[36m"
+COBALT = "\33[34m"
+
+# Yellow (ForeGround)
+YELLOW = "\33[33m"
+MUSTARD = "\33[93m"
+
+# Purple (ForeGround)
+VIOLET = "\33[95m"
+PURPLE = "\33[35m"
+
+# GreyScale (ForeGround)
+WHITE = "\33[97m"
+GREY = "\33[37m"
+SILVER = "\33[90m"
+BLACK = "\33[30m"
