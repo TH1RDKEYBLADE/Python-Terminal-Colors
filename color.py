a955f4db1142ea0f8298afff53152fd84bdd645e
Kiwi <88976806+TH1RDKEYBLADE@users.noreply.github.com>
2022-10-15 16:53:11 -0400

    Added Colors to the "Background(BG)" Category.

--- color.py
+++ color.py
@@ -39,3 +39,31 @@ WHITE = "\33[97m"
 GREY = "\33[37m"
 SILVER = "\33[90m"
 BLACK = "\33[30m"
+
+# Red (BG)
+PINK_BG = "\33[101m"
+RED_BG = "\33[41m"
+
+# Green (BG)
+LIME_BG = "\33[102m"
+GREEN_BG = "\33[42m"
+
+# Blue (BG)
+AQUA_BG = "\33[46m"
+BLUE_BG = "\33[44m"
+CYAN_BG = "\33[106m"
+COBALT_BG = "\33[104m"
+
+# Yellow (BG)
+YELLOW_BG = "\33[103m"
+MUSTARD_BG = "\33[43m"
+
+# Purple (BG)
+PURPLE_BG = "\33[45m"
+VIOLET_BG = "\33[105m"
+
+# GreyScale (BG)
+WHITE_BG = "\33[107m"
+GREY_BG = "\33[47m"
+SILVER_BG = "\33[100m"
+BLACK_BG = "\33[40m"
